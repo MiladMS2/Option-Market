4af67e4001b45f470fd64db8e7a885d941bb6fcf
unknown <ebrahim.dadgar@gmail.com~>
2023-08-26 23:53:27 +0330

    Fixed expiration dates in option function

--- Clean&Combine.R
+++ Clean&Combine.R
@@ -1,3 +1,5 @@
+require(janitor)
+
 # Cleaning Function -------------------------------------------------------
 # Reading and cleaning first row,add date to 1st column
 tsetmc_cleaner <- function(file_date) {
@@ -25,5 +27,22 @@ tsetmc_combiner <- function(file_list) {
 
 # Option_trades -----------------------------------------------------------
 
+tsetmc_option <- function(file_date) {
+  x <- tsetmc_combiner(file_date) %>%
 # Find option trades
-x <- tsetmc_combiner("1402-05-31")
+    filter(grepl("اختیار", نام)) %>%
+# Select the needed cols
+    select(c("date", "نماد", "نام", "ارزش", "آخرین معامله - مقدار", "قیمت پایانی - مقدار")) %>%
+    clean_names() %>%
+# Add K,UA,EXP_date cols
+    mutate(
+      option_type = sub("^(اختیار[فخ]) .*", "\\1", nam, perl = TRUE), .after = date,
+      K = as.numeric(sub("^.*-(\\d+)-.*", "\\1", nam)),
+      UA = str_extract(nam, "\\s(.*?)-", group = 1),
+      Exp_date = str_extract(nam, "(?<=-)[^-]+$") %>% 
+        str_remove_all("/") %>% 
+        str_sub(-6)
+    ) %>% mutate(Exp_date=paste0("1402-",substring(Exp_date,3,4),"-",substring(Exp_date,5,6)))
+  return(x)
+}
+
